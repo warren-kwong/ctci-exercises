25e2551095de4af5adce6af71a5b38f514744701
Warren <warren.p.kwong@gmail.com>
2018-11-14 23:32:36 -0800

    stringCompress, rotateMatrix, zeroMatrix, stringRotation tests

--- problems/ch01.js
+++ problems/ch01.js
@@ -52,13 +52,37 @@ const urlify = s => {
 
 // 1.4 Palindrome Permutation
 // **************************************
-// Give a string, write a function to check if it is a permutation of a plandrime. A palindrome is a word or phrase that is the same forwards and backwards. A permutation is a rearrangement of the letters. The palindrome does not need to be limited to just dictionary words.
+// Give a string, write a function to check if it is a permutation of a palindrome. A palindrome is a word or phrase that is the same forwards and backwards. A permutation is a rearrangement of the letters. The palindrome does not need to be limited to just dictionary words.
 const palindromePermutation = s => {};
 
 // 1.5 One Away
 // **************************************
 // There are three types of edits that can be performed on strings: insert a character, remove a character, or replace a character.  Given two strings, write a function to check if they are one edit (or zero edits) away
-const oneAway = (s1, s2) => {};
+const oneAway = (s1, s2) => {
+
+
+};
+
+// 1.6 String Compression
+// **************************************
+// Implement a method to perform basic string compression using the counts of repeated characters.  For example, the string aabcccccaaa would become a2b1c5a3.  If the "compressed" string would not become smaller than the original string, your method should return the original string. You can assume the string has only uppercase and lowercase letters (a-z)
+const stringCompression = s => {};
+
+// 1.7 Rotate Matrix
+// **************************************
+// Given an image represeted by an NxN matrix, where each pixel in the image is 4 bytes, write a method to rotate the image by 90 degrees. 
+// Can you do this in place?
+const rotateMatrix = matrix => {};
+
+// 1.8 Zero Matrix
+// **************************************
+// Write an algorithm such that if an element in an MxN matrix is 0, its entire row and column are set to 0.
+const zeroMatrix = matrix => {};
+
+// 1.9 String Rotation
+// **************************************
+// Assume you have a method isSubstring which checks if one word is a substring of another. Given two strings, s1 and s2, write code to check i f s2 is a rotation of s1 using only one call to isSubstring(e.g. "waterbottle" is a rotation of "erbottlewat").
+const stringRotation = (s1, s2) => {}
 
 // **************************************
 module.exports = {
@@ -67,5 +91,6 @@ module.exports = {
   checkPermutation,
   urlify,
   palindromePermutation,
-  oneAway
+  oneAway,
+  stringCompression
 };
--- test/ch01_test.js
+++ test/ch01_test.js
@@ -8,7 +8,11 @@ const {
   checkPermutation,
   urlify,
   palindromePermutation,
-  oneAway
+  oneAway,
+  stringCompression,
+  rotateMatrix,
+  zeroMatrix,
+  stringRotation
 } = require('../problems/ch01');
 
 describe('ch01', function() {
@@ -39,25 +43,99 @@ describe('ch01', function() {
 
   describe('URLify', function() {
     it("should replace all spaces in a string with '%20'", function() {
-      urlify('Hello World').should.equal.to('Hello%20World');
-      urlify('What a nice day today').should.equal.to(
+      urlify('Hello World').should.equal('Hello%20World');
+      urlify('What a nice day today').should.equal(
         'What%20a%20nice%20day%20today'
       );
     });
   });
 
   describe('palindromePermutation', function() {
-    it('should check if it is a permutation of a palindrome', function() {
+    xit('should check if it is a permutation of a palindrome', function() {
       palindromePermutation('Tact Coa').should.equal(true);
     });
   });
 
   describe('oneAway', function() {
-    it('should check if the two strings are one edit (or zero edits) away from each other', function() {
+    xit('should check if the two strings are one edit (or zero edits) away from each other', function() {
       oneAway('pale', 'ple').should.equal(true);
       oneAway('pales', 'pale').should.equal(true);
       oneAway('pale', 'bale').should.equal(true);
       oneAway('pale', 'bake').should.equal(false);
     });
   });
+
+  describe('stringCompression', function() {
+    xit('should "compress" the input string \(ex. aabcccccaaaa becomes a2b1c5a3\)', function() {
+      stringCompression('aaaaaaa').should.equal('a7');
+      stringCompression('aabcccccaaa').should.equal('a2b1c5a3');
+    });
+    xit('should return the original input if the compressed string is longer than the original string', function() {
+      stringCompression('a').should.equal('a');
+      stringCompression('abcdefg').should.equal('abcdefg');
+    });
+  });
+
+  describe('rotateMatrix', function() {
+    const matrix1 = [
+      [1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9]
+    ];
+
+    const matrix1answer = [
+      [7, 4, 1],
+      [8, 5, 2],
+      [9, 6, 3]
+    ]
+  
+    xit('', function() {
+      rotateMatrix(matrix1).should.equal(matrix1answer);
+    })
+  });
+
+  describe('zeroMatrix', function() {
+    const matrix1 = [
+      [1, 2, 3], 
+      [4, 5, 6], 
+      [7, 8, 9]
+    ];
+
+    const matrix2 = [
+      [0, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 0]
+    ]
+
+    const matrix2answer = [
+      [0, 0, 0, 0, 0],
+      [0, 1, 1, 1, 0],
+      [0, 0, 0, 0, 0]
+    ]
+
+    const matrix3 = [
+      [0, 1],
+      [1, 1],
+      [1, 1],
+    ]
+
+    const matrix3answer = [
+      [0, 0],
+      [0, 1],
+      [0, 1],
+    ]
+
+    xit('', function() {
+      zeroMatrix(matrix1).should.equal(matrix1);
+      zeroMatrix(matrix2).should.equal(matrix2answer);
+      zeroMatrix(matrix3).should.equal(matrix3answer);
+    })
+  });
+
+  describe('stringRotation', function() {
+    xit('', function() {
+      stringRotation().should.equal();
+    })
+  })
+
 });
